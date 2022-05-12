924dfd1a512164e38f71a9e8f76c6bb59d114dbb
nishu24 <neesarg@gmail.com>
2022-05-11 21:53:42 -0700

    initial code structure

---
+++ src/main/java/com/revature/ProjectDemoJenkins.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDemoJenkins {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDemoJenkins.class, args);
+	}
+
+}
